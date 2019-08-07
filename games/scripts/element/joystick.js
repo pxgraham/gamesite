cb3442a5d62305bc3d356ec4351368f52b9e3e6d
Peyton <41251008+pxgraham@users.noreply.github.com>
2019-08-06 21:39:10 -0700

    fixed some controls

--- games/scripts/element/joystick.js
+++ games/scripts/element/joystick.js
@@ -1,33 +1,47 @@
-var joystickpad = document.getElementById('joystick');
-var jctx = joystickpad.getContext('2d');
-var rect = joystickpad.getBoundingClientRect();
+var pad = document.getElementById('joystick');
+pad.width = 350;
+pad.height = 350;
+var jctx = pad.getContext('2d');
+var rect = pad.getBoundingClientRect();
 
-var cX = Math.floor(joystickpad.width / 2);
-var cY = Math.floor(joystickpad.height / 2);
+jctx.fillStyle = 'black';
+jctx.fillRect(116, 0, 10, pad.height);
+jctx.fillRect(233, 0, 10, pad.height);
+
+jctx.fillRect(0, 116, pad.width, 10);
+jctx.fillRect(0, 233, pad.width, 10);
+
+var cX = Math.floor(pad.width / 2);
+var cY = Math.floor(pad.height / 2);
 
 var xPos;
 var yPox;
 var des;
 var dist;
 
-// joystickpad.addEventListener('touchstart', function(e) {
+// pad.addEventListener('touchstart', function(e) {
     
 // })
+var initialX;
+var initialY;
 
 
-
-function getMousePos(joystickpad, mouseEvent) {
-    var rect = joystickpad.getBoundingClientRect();
+function getMousePos(pad, mouseEvent) {
+    var rect = pad.getBoundingClientRect();
+    
+    initialX = mouseEvent.touches[0].clientX - 80;
+    initialY = mouseEvent.touches[0].clientY - 800;
+    
     return {
-      x: mouseEvent.touches[0].clientX - rect.left,
-      y: mouseEvent.touches[0].clientY - rect.top
+      x: mouseEvent.touches[0].clientX - 80,
+      y: mouseEvent.touches[0].clientY - 800
     };
 }
 
 var interval;
-joystickpad.addEventListener("touchmove", function(e) {
+pad.addEventListener("touchmove", function(e) {
                        
-        var m = getMousePos(joystickpad, e);
+        var m = getMousePos(pad, e);
         
         if (m.x < cX) {
           if (m.y < cY) pos = 180 + Math.floor(Math.atan((m.y - cY) / (cX - m.x)) * (180 / Math.PI));
@@ -75,14 +89,24 @@ Distance:   ${tD}px ${cX} 30
   }, false);
 
 function joystickrender() {
+    console.log(initialX, initialY)
+
     jctx.fillStyle = 'lime',
-    jctx.fillRect(0, 0, joystickpad.width, joystickpad.height);
-    jctx.fillStyle = 'lime',
-    jctx.fillRect(xPos, yPos, 15, 15);
+    jctx.fillRect(0, 0, pad.width, pad.height);
+    jctx.fillStyle = 'black';
+    jctx.fillRect(116, 0, 10, pad.height);
+    jctx.fillRect(233, 0, 10, pad.height);
+
+    jctx.fillRect(0, 116, pad.width, 10);
+    jctx.fillRect(0, 233, pad.width, 10);
+
+    jctx.fillStyle = 'black',
+    // jctx.fillRect(pad.width/2 - 7.5, pad.height/2 - 7.5, 15, 15);
+    jctx.fillRect(initialX, initialY, 15, 15);
 }
 
-joystickpad.addEventListener('touchstart', function(e) {
-    var m = getMousePos(joystickpad, e);
+pad.addEventListener('touchstart', function(e) {
+    var m = getMousePos(pad, e);
         
     if (m.x < cX) {
       if (m.y < cY) pos = 180 + Math.floor(Math.atan((m.y - cY) / (cX - m.x)) * (180 / Math.PI));
@@ -100,25 +124,68 @@ joystickpad.addEventListener('touchstart', function(e) {
     yPos = m.y - 80;
     deg = pos;
     dist = tD;
-    if(xPos > 169) {
-        right = true;
-        left = false;
-    }
-    if(xPos < 169) {
+
+    //upper left corner
+    if(initialX < 116 && initialY < 116 && initialY) {
         left = true;
-        right = false;
-    }
-    if(yPos < 179) {
         up = true;
         down = false;
+        right = false;
     }
-    if(yPos > 179) {
+
+    //center left
+    if(initialX < 116 && initialY > 116 && initialY < 233) {
+        left = true;
         up = false;
-        down = true;
+        down = false;
+        right = false;
     }
+    // bottom left
+    if(initialY > 233 && initialX < 116) {
+        down = true;
+        left = true;
+        up = false;
+        right = false;
+    }
+    //bottom center;
+    if(initialX > 116 && initialX < 233 && initialY > 266) {
+        down = true;
+        up = false;
+        left = false;
+        right = false;
+    }
+    //bottom right
+    if(initialX > 266 && initialY > 266) {
+        right = true;
+        down = true;
+        left = false;
+        up = false;
+    }
+    //right center
+    if(initialX > 266 && initialY > 116 && initialY < 266) {
+        right = true;
+        down = false;
+        left = false;
+        up = false;
+    }
+    //upper right
+    if(initialX > 266 && initialY < 116) {
+        up = true;
+        right = true;
+        left = false;
+        down = false;
+    }
+    //top center 
+    if(initialX > 133 && initialX < 266 && initialY < 116) {
+        up = true;
+        right = false;
+        left = false;
+        down = false;
+    }
+    joystickrender()
 })
 
-joystickpad.addEventListener('touchend', function(e) {
+pad.addEventListener('touchend', function(e) {
     up = false;
     down = false;
     left = false;
