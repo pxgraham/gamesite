28f92147e4e73a4b551b7e70c1beb3bee3d89017
Peyton <41251008+pxgraham@users.noreply.github.com>
2019-08-06 22:04:19 -0700

    fixed some controls

--- games/scripts/element/joystick.js
+++ games/scripts/element/joystick.js
@@ -11,6 +11,8 @@ jctx.fillRect(233, 0, 10, pad.height);
 jctx.fillRect(0, 116, pad.width, 10);
 jctx.fillRect(0, 233, pad.width, 10);
 
+jctx.fillRect(116, 116, 117, 117)
+
 var cX = Math.floor(pad.width / 2);
 var cY = Math.floor(pad.height / 2);
 
@@ -69,21 +71,61 @@ Distance:   ${tD}px ${cX} 30
 
     joystickrender()
 
-    if(xPos > 169) {
-        right = true;
-        left = false;
-    }
-    if(xPos < 169) {
+    if(initialX < 116 && initialY < 116 && initialY) {
         left = true;
-        right = false;
-    }
-    if(yPos < 179) {
         up = true;
         down = false;
+        right = false;
     }
-    if(yPos > 179) {
+
+    //center left
+    if(initialX < 116 && initialY > 116 && initialY < 233) {
+        left = true;
         up = false;
+        down = false;
+        right = false;
+    }
+    // bottom left
+    if(initialY > 233 && initialX < 116) {
         down = true;
+        left = true;
+        up = false;
+        right = false;
+    }
+    //bottom center;
+    if(initialX > 116 && initialX < 233 && initialY > 266) {
+        down = true;
+        up = false;
+        left = false;
+        right = false;
+    }
+    //bottom right
+    if(initialX > 266 && initialY > 266) {
+        right = true;
+        down = true;
+        left = false;
+        up = false;
+    }
+    //right center
+    if(initialX > 266 && initialY > 116 && initialY < 266) {
+        right = true;
+        down = false;
+        left = false;
+        up = false;
+    }
+    //upper right
+    if(initialX > 266 && initialY < 116) {
+        up = true;
+        right = true;
+        left = false;
+        down = false;
+    }
+    //top center 
+    if(initialX > 133 && initialX < 266 && initialY < 116) {
+        up = true;
+        right = false;
+        left = false;
+        down = false;
     }
         
 
@@ -97,6 +139,7 @@ function joystickrender() {
     jctx.fillStyle = 'black';
     jctx.fillRect(116, 0, 10, pad.height);
     jctx.fillRect(233, 0, 10, pad.height);
+    jctx.fillRect(116, 116, 117, 117)
 
     jctx.fillRect(0, 116, pad.width, 10);
     jctx.fillRect(0, 233, pad.width, 10);
