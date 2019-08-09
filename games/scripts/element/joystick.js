ccb26fef91900268d5c6ac806cc1eb1df30b0b5c
Peyton <41251008+pxgraham@users.noreply.github.com>
2019-08-08 21:06:24 -0700

    fix

--- games/scripts/element/joystick.js
+++ games/scripts/element/joystick.js
@@ -46,6 +46,10 @@ function getMousePos(pad, mouseEvent) {
     }  else if(mouseEvent.touches.length > 1) {
         initialX = mouseEvent.touches[1].clientX - 80;
         initialY = mouseEvent.touches[1].clientY - 800;
+        if(initialx > 350 || initialy > 350 || initialX < 0 || initialY < 0) {
+            initialX = mouseEvent.touches[0].clientX - 80;
+            initialY = mouseEvent.touches[0].clientY - 800;
+        }
     }
 
     console.log('moust event touches --- ' + mouseEvent.touches.length);
