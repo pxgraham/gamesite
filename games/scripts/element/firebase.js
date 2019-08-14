ce5c96a11add0d2e3a495d707349b070537f83bd
Peyton <41251008+pxgraham@users.noreply.github.com>
2019-08-13 20:42:35 -0700

    added session

--- games/scripts/element/firebase.js
+++ games/scripts/element/firebase.js
@@ -13,30 +13,95 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 var playerInfo = database.ref("/playerInfo")
+var player1active = false;
+var playerid;
 
+var currentPlayers = null;
 
 playerInfo.on("value", function(snapshot) {
+    player1active = snapshot.child('player1').child('active').val();
 
-    playerInfo.child('position').child('x').set(player.x);
-    playerInfo.child('position').child('y').set(player.y);
 
-    updateGameWithDbInfo(snapshot);
+    player.x = snapshot.child('player1').child('position').child('x').val();
+    player.y = snapshot.child('player1').child('position').child('y').val();
+
+    console.log(snapshot.child('player1').child('position').child('x').val());
 
 })
 
-$('#testBtn').on('click', function() {
-    updateDb();
-    // updateGameWithDbInfo();
+$('#connectBtn').on('click', function() {
+
+    if(player1active) {
+        alert('players already here')
+    } else {
+        playerid = 'first';
+        playerInfo.child('player1').child('active').set(true);
+        alert('you joined!')
+    }
+
 })
 
-function updateDb() {
-    playerInfo.child('position').set({
-        x: player.x,
-        y: player.y
-    })
-}
+$('#disconnectBtn').on('click', function() {
+    playerInfo.child('player1').child('active').set(false);
+    alert('you lost control')
+})
 
-function updateGameWithDbInfo(snapshot) {
-        player.x = snapshot.child('position').child('x').val();
-        player.y = snapshot.child('position').child('y').val();
-}
+$('#updateBtn').on('click', function() {
+
+    if(playerid === 'first') {
+        playerInfo.child('player1').child('position').set({
+            x: player.x,
+            y: player.y
+        })
+    } else {
+        alert('your not in control');
+    }    
+
+})
+
+function getInGame() {
+    // Checks for current players, if theres a player one connected, then the user becomes player 2.
+    // If there is no player one, then the user becomes player 1
+    if (currentPlayers < 2) {
+  
+      if (playerOneExists) {
+        playerNum = 2;
+      }
+      else {
+        playerNum = 1;
+      }
+  
+      // Creates key based on assigned player number
+      playerRef = database.ref("/players/" + playerNum);
+  
+      // Creates player object. 'choice' is unnecessary here, but I left it in to be as complete as possible
+      playerRef.set({
+        name: username,
+        wins: 0,
+        losses: 0,
+        choice: null
+      });
+  
+      // On disconnect remove this user's player object
+      playerRef.onDisconnect().remove();
+  
+      // If a user disconnects, set the current turn to 'null' so the game does not continue
+      currentTurnRef.onDisconnect().remove();
+  
+      // Send disconnect message to chat with Firebase server generated timestamp and id of '0' to denote system message
+      chatDataDisc.onDisconnect().set({
+        name: username,
+        time: firebase.database.ServerValue.TIMESTAMP,
+        message: "has disconnected.",
+        idNum: 0
+      });
+  
+      // Remove name input box and show current player number.
+      $("#swap-zone").html("<h2>Hi " + username + "! You are Player " + playerNum + "</h2>");
+    }
+    else {
+  
+      // If current players is "2", will not allow the player to join
+      alert("Sorry, Game Full! Try Again Later!");
+    }
+  }
